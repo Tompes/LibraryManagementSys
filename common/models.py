8fb433c973f1801f65e55dde0a3345bf73cd726b
Tompes <ghoulcn@vip.qq.com>
2018-12-02 12:35:45 +0800

    Generate Database models

---
+++ common/models.py
@@ -0,0 +1,82 @@
+# coding: utf-8
+from sqlalchemy import Column, DateTime, Float, ForeignKey, Integer, LargeBinary, Numeric, SmallInteger, Text, Unicode
+from sqlalchemy.dialects.mssql.base import BIT, MONEY
+from sqlalchemy.schema import FetchedValue
+from sqlalchemy.orm import relationship
+from flask_sqlalchemy import SQLAlchemy
+
+
+db = SQLAlchemy()
+
+
+class TbBook(db.Model):
+    __tablename__ = 'Tb_Book'
+
+    bkID = db.Column(db.Integer, primary_key=True)
+    bkCode = db.Column(db.Unicode(20))
+    bkName = db.Column(db.Unicode(50))
+    bkAuthor = db.Column(db.Unicode(30))
+    bkPress = db.Column(db.Unicode(50))
+    bkDatePress = db.Column(db.DateTime)
+    bkISBN = db.Column(db.Unicode(15))
+    bkCatalog = db.Column(db.Unicode(30))
+    bkLanguage = db.Column(db.SmallInteger)
+    bkPages = db.Column(db.Integer)
+    bkPrice = db.Column(db.MONEY)
+    bkDateIn = db.Column(db.DateTime)
+    bkBrief = db.Column(db.Text(2147483647, 'Chinese_PRC_CI_AS'))
+    bkCover = db.Column(db.LargeBinary)
+    bkStatus = db.Column(db.Unicode(2))
+
+
+class TbBorrow(db.Model):
+    __tablename__ = 'Tb_Borrow'
+
+    BorrowID = db.Column(db.Numeric(12, 0), primary_key=True)
+    rdID = db.Column(db.ForeignKey('Tb_Reader.rdID'))
+    bkID = db.Column(db.ForeignKey('Tb_Book.bkID'))
+    ldContinueTimes = db.Column(db.Integer, server_default=db.FetchedValue())
+    ldDateOut = db.Column(db.DateTime)
+    ldDateRetPlan = db.Column(db.DateTime)
+    ldDateRetAct = db.Column(db.DateTime)
+    ldOverDay = db.Column(db.Integer)
+    ldOverMoney = db.Column(db.MONEY)
+    ldPunishMoney = db.Column(db.MONEY)
+    lsHasReturn = db.Column(db.BIT, server_default=db.FetchedValue())
+    OperatorLend = db.Column(db.Unicode(20))
+    OperatorRet = db.Column(db.Unicode(20))
+
+    Tb_Book = db.relationship('TbBook', primaryjoin='TbBorrow.bkID == TbBook.bkID', backref='tb_borrows')
+    Tb_Reader = db.relationship('TbReader', primaryjoin='TbBorrow.rdID == TbReader.rdID', backref='tb_borrows')
+
+
+class TbReader(db.Model):
+    __tablename__ = 'Tb_Reader'
+
+    rdID = db.Column(db.Integer, primary_key=True)
+    rdName = db.Column(db.Unicode(20))
+    rdSex = db.Column(db.Unicode(1))
+    rdType = db.Column(db.ForeignKey('Tb_ReaderType.rdType'))
+    rdDept = db.Column(db.Unicode(20))
+    rdPhone = db.Column(db.Unicode(25))
+    rdEmail = db.Column(db.Unicode(25))
+    rdDateReg = db.Column(db.DateTime)
+    rdPhoto = db.Column(db.LargeBinary)
+    rdStatus = db.Column(db.Unicode(2))
+    rdBorrowQty = db.Column(db.Integer, server_default=db.FetchedValue())
+    rdPwd = db.Column(db.Unicode(20), server_default=db.FetchedValue())
+    rdAdminRoles = db.Column(db.SmallInteger, server_default=db.FetchedValue())
+
+    Tb_ReaderType = db.relationship('TbReaderType', primaryjoin='TbReader.rdType == TbReaderType.rdType', backref='tb_readers')
+
+
+class TbReaderType(db.Model):
+    __tablename__ = 'Tb_ReaderType'
+
+    rdType = db.Column(db.SmallInteger, primary_key=True)
+    rdTypeName = db.Column(db.Unicode(20), nullable=False, unique=True)
+    CanLendQty = db.Column(db.Integer)
+    CanLendDay = db.Column(db.Integer)
+    CanContinueTimes = db.Column(db.Integer)
+    PunishRate = db.Column(db.Float(53))
+    DateValid = db.Column(db.SmallInteger, nullable=False)
